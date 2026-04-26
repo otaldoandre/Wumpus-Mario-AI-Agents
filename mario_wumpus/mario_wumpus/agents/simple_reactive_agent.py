89f3d48e1e84811eddba6a880c20b8ddbc2f2879
otaldoandre <andrealves431@hotmail.com>
2026-04-26 13:56:49 -0300

    Improve simple reactive agent
    - Handle breeze perception by moving the agent in the opposite direction.
    - Add a randomness to the breeze perception functionality to prevent the infinite loops that occur when 2 pits are near the agent
    - Avoid actions that decrease score by changing the last return statement

--- mario_wumpus/mario_wumpus/agents/simple_reactive_agent.py
+++ mario_wumpus/mario_wumpus/agents/simple_reactive_agent.py
@@ -4,6 +4,7 @@ import random
 
 from .base import BaseAgent
 from ..core.actions import Action
+from ..core.actions import Direction
 from ..core.models import Percept
 
 
@@ -21,11 +22,28 @@ class ReactiveAgent(BaseAgent):
         print("Agente Reativo!")
 
     def act(self, percept: Percept, legal_actions: list[Action]) -> Action:
+        print(percept.facing)
         if percept.glitter:
             return Action.RESCUE
 
         if percept.has_fireball and percept.stink:
             return Action.SHOOT
 
-        return self.rng.choice(legal_actions)
+        # Lida com a percepção do Breeze com base na informação de direção da percepção atual.
+        if percept.breeze:
+            opposite_direction = {
+                Direction.UP: Action.MOVE_DOWN,
+                Direction.RIGHT: Action.MOVE_LEFT,
+                Direction.DOWN: Action.MOVE_UP,
+                Direction.LEFT: Action.MOVE_RIGHT,
+            }
+
+            # Lida com o bug de loop infinito quando há dois poços adjacentes adicionando aleatoriedade
+            if opposite_direction[percept.facing] in legal_actions and self.rng.random() > 0.05:
+                return opposite_direction.get(percept.facing)
+
+        # Evita tomar ações que retirem pontos como mirar e esperar
+        return self.rng.choice(
+            [Action.MOVE_UP, Action.MOVE_RIGHT, Action.MOVE_DOWN, Action.MOVE_LEFT]
+        )
 
